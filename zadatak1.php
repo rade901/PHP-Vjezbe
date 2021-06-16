f1d5626f1df656dc91f9b6d1efa1886bb95a7d06
Rade Jasenovcanin <78330407+rade901@users.noreply.github.com>
2021-06-17 01:45:25 +0200

    zadatak 1
    if ,eseif,else, vjezba

---
+++ zadatak1.php
@@ -0,0 +1,13 @@
+<?php
+$godina =21;
+
+if ($godina >21){
+    echo "imas vise od 21 godinu";
+}
+elseif ( $godina == 21 )
+{
+    echo "imas tacno 21 godinu";
+}
+else{
+    echo "imas manje od 21 godinu";
+}
