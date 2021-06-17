c61331be02aecf552d2d62e8d06d98c079d2905e
Rade Jasenovcanin <78330407+rade901@users.noreply.github.com>
2021-06-17 02:49:40 +0200

    switch

---
+++ switch.php
@@ -0,0 +1,19 @@
+<?php
+$t = 1;
+$x = 2;
+
+switch ($x) {
+    case 1:
+            echo "dobar jutro";
+        break;
+        case 'dan':
+            echo "dobar dan";
+        break;
+        case 2:
+            echo "dobro vece";
+        break;
+}
+//ispisat ce dobro vece zato sto je switch ($x)
+
+
+
